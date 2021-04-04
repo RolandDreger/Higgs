c8ed25fa021fdd7d515c413e697dc474c1a4ad6b
Roland Dreger <mail@rolanddreger.net>
2021-04-04 16:01:48 +0200

    Change folder structure (public folder)

--- site/config/config.php
+++ site/config/config.php
@@ -3,5 +3,8 @@
 return [
   'debug' => false,
 	'languages' => true,
-	'date.handler' => 'strftime'
+	'date.handler' => 'strftime',
+	'api' => require_once 'api.php',
+	'hooks' => require_once 'hooks.php',
+	'routes' => require_once 'routes.php'
 ];
