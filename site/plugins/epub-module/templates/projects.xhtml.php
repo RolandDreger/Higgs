ba8ebb4fcd6ca97beb1cfeb01d20867c2c654dd2
Roland Dreger <mail@rolanddreger.net>
2021-05-03 12:35:24 +0200

    Add snippets

--- site/plugins/epub-module/templates/projects.xhtml.php
+++ site/plugins/epub-module/templates/projects.xhtml.php
@@ -1,19 +1,8 @@
-<?php
-	header('Content-type: text/xml; charset="utf-8"');
-	echo '<?xml version="1.0" encoding="utf-8" ?>';
-?>
-
+<?php snippet('epub/content-type'); ?>
 <html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops" xml:lang="en" lang="en">
-	<head>
-		<meta charset="utf-8" />
-		<title><?= $page->title(); ?></title>
-		
-		<link rel="stylesheet" type="text/css" href="stylesheet.css" id="style-standard" />
-	</head>
-	<body epub:type="bodymatter chapter">
-		<main class="main">
-			<h1><?= $site->title(); ?></h1>
-			<?= $page->text() ?>
-		</main>
+<?php snippet('epub/head'); ?>
+	<body>
+		<h1><?= $site->title(); ?></h1>
+		<p><?= $page->title(); ?></p>
 	</body>
 </html>
