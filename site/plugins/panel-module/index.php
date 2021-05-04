39a71c6827a37c47d5d2cac530fb7f91e8f437de
Roland Dreger <mail@rolanddreger.net>
2021-05-04 22:43:17 +0200

    Add panel js and css

--- site/plugins/panel-module/index.php
+++ site/plugins/panel-module/index.php
@@ -1,7 +1,7 @@
 <?php
 
 Kirby::plugin('higgs/panel-module', [
-	
+
 	/* Translate field values with options in language files (e.g. site/language/en.php) */
 	'fieldMethods' => [
 		'translate' => function ($field) {
