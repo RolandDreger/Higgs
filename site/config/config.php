ae5853375e5ea63a46244aa297f3db37d37cc21c
Roland Dreger <mail@rolanddreger.net>
2021-05-04 23:43:43 +0200

    Add content config file

--- site/config/config.php
+++ site/config/config.php
@@ -4,6 +4,7 @@ return [
   'debug' => false,
 	'languages' => true,
 	'date.handler' => 'strftime',
+	'content' => require_once 'content.php',
 	'api' => require_once 'api.php',
 	'hooks' => require_once 'hooks.php',
 	'routes' => require_once 'routes.php'
---
+++ site/config/content.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+	/* Set the extension you want to use for your content files. Values: txt | md */
+	/* 'extension' => 'txt', */
+	/* Set an array of file/folder names that are not scanned by Kirby's core. */
+	/* 'ignore' => [] */
+];
