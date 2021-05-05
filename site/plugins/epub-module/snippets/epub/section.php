ca5368648bf54792a02a2f3f69627d4e1b02316e
Roland Dreger <mail@rolanddreger.net>
2021-05-05 23:28:26 +0200

    Add epub attributes

--- site/plugins/epub-module/snippets/epub/section.php
+++ site/plugins/epub-module/snippets/epub/section.php
@@ -1,11 +1,21 @@
-<section 
-	epub:type="<?= trim($targetPage->documentPart() . ' ' . $targetPage->documentRole()); ?>" 
-	role="<?= $targetPage->documentRole(); ?>"
->
+<?php
+	$docPart = trim($targetPage->documentPart());
+	$docRole = trim($targetPage->documentRole());
+	$docType = trim($targetPage->documentType());
+	$epubTypeArray = array_filter([$docPart, $docRole], function($item) { 
+		return !empty($item); 
+	});
+	$epupType = implode(' ', $epubTypeArray);
+	$docRole = trim($targetPage->documentRole());
+	$docTitle = $targetPage->title();
+	$docText = $targetPage->documentText();
+?>
+
+<section <?= Xml::attr(['epub:type' => $epupType, 'role' => $docRole]); ?>>
 	<header>
-		<h1 epub:type="title"><?= $targetPage->title(); ?></h1>
+		<h1 epub:type="title"><?= $docTitle; ?></h1>
 	</header>
-	<?php if($targetPage->documentText()->isNotEmpty()): ?>
-		<p><?= esc($targetPage->documentText(), 'html'); ?></p>
+	<?php if($docText->isNotEmpty()): ?>
+		<?= markdown($docText, 'html'); ?>
 	<?php endif; ?>
 </section>
