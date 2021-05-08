489c4dfc9d69dc8768daa41458d687ec43c2caac
Roland Dreger <mail@rolanddreger.net>
2021-05-08 13:07:46 +0200

    Change string check

--- site/plugins/panel-module/index.php
+++ site/plugins/panel-module/index.php
@@ -6,27 +6,33 @@ Kirby::plugin('higgs/panel-module', [
 	'fieldMethods' => [
 
 		/* 
-			Translate field values with options in language files
-			e.g. site/language/en.php
+		 * Translate field values with options in language files
+		 * e.g. site/language/en.php
+		 * 
+		 * @param {Kirby\CMS\Field} $field
+		 * @returns {}
 		*/
 		'translate' => function ($field) {
 			
 			$value = $field->value();
 
-			if(is_string($value)) {
-				/* Check: Comma separated string? */
-				if(preg_match('/[,;]/', $value, $matches)) {
-					$separator = $matches[0];
-					$valueArray = explode($separator, $value);
-					$resultArray = [];
-					foreach($valueArray as $curValue) {
-						array_push($resultArray, t(trim($curValue), $value));
-					}
-					$field->value = implode($separator, $resultArray);
-				} else {
-					$field->value = t($value, $value);
+			/* Check: Field value is a string? */
+			if(!is_string($value)) {
+				return $field;
+			}
+				
+			/* Check: Comma separated string? */
+			if(preg_match('/[,;]/', $value, $matches)) {
+				$separator = $matches[0];
+				$valueArray = explode($separator, $value);
+				$resultArray = [];
+				foreach($valueArray as $curValue) {
+					array_push($resultArray, t(trim($curValue), $value));
 				}
-			} 
+				$field->value = implode($separator, $resultArray);
+			} else {
+				$field->value = t($value, $value);
+			}
 			
       return $field;
 		}
