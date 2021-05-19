1be76cc6f099d4491a3961f138a7e5e473d6b136
Roland Dreger <mail@rolanddreger.net>
2021-05-19 10:05:26 +0200

    Add css links to xhtml head

--- site/plugins/epub-module/snippets/head.php
+++ site/plugins/epub-module/snippets/head.php
@@ -2,20 +2,38 @@
 	<meta charset="UFT-8" />
 	<title><?= $page->title(); ?></title>
 	<?php
-		/* projektPage selbst berücksichtigen -> als pageMethod? */
-		$parentPages = $page->parents();
-		foreach($parentPages as $parent) {
-			if($cssFilesField = $parent->epubCSSFiles()) {
-				if($cssFilesField->exists()) {
-					if($cssFilesField->isNotEmpty()) {
-						$cssFiles = $cssFilesField->toFiles();
-						foreach($cssFiles as $css) {
-							echo css($css->url()) . '</link>';
-						}
-					}
-					break;
+		
+		$cssFiles = new Collection([]);
+
+		/* CSS files of page */
+		if($cssFilesField = $page->epubCSSFiles()) {
+			if($cssFilesField->exists()) {
+				if($cssFilesField->isNotEmpty()) {
+					$cssFiles->add($cssFilesField->toFiles());
 				}
 			}
 		}
+		
+		/* CSS files of project pages */
+		$parentPages = $page->parents();
+			
+		foreach($parentPages as $parentPage) {
+			if($parentPage->intendedTemplate()->name() !== 'project') {
+				continue;
+			}
+			if($cssFilesField = $parentPage->epubCSSFiles()) {
+				if($cssFilesField->exists()) {
+					if($cssFilesField->isNotEmpty()) {
+						$cssFiles->add($cssFilesField->toFiles());
+						break;
+					}
+				}
+			}
+		}
+		
+		/* Add stylesheet link elements */
+		foreach($cssFiles as $css) {
+			echo css($css->url()) . '</link>';
+		}		
 	?>
 </head>
