e033e76df761444cae2bbe5a8178032729eeb189
Roland Dreger <mail@rolanddreger.net>
2021-05-19 10:41:22 +0200

    Add styles to html documents

--- site/snippets/html/header.php
+++ site/snippets/html/header.php
@@ -1,7 +1,9 @@
 <header class="header">
 	<?php if($logo = $site->file('logo.svg')): ?>
 		<a id="site-logo" href="<?php echo $site->url(); ?>" aria-label="Go to Homepage">
+			<?php if($page->isHomePage()): ?>
 			<?php echo $logo->read(); ?>
+			<?php endif; ?>
 		</a>
 	<?php endif; ?>
 </header>
